c42a65d2e0dc3eb397db61d02d815473c46c7263
Mateo Morales García <mateomogar@gmail.com>
2021-02-19 14:39:40 -0500

    Add base for SO events architecture
    Taken from the open project 1 :)

---
+++ Assets/Scripts/Events/ScriptableObjects/IntEventChannelSO.cs
@@ -0,0 +1,19 @@
+using UnityEngine.Events;
+using UnityEngine;
+
+/// <summary>
+/// This class is used for Events that have one int argument.
+/// Example: An Achievement unlock event, where the int is the Achievement ID.
+/// </summary>
+
+[CreateAssetMenu(menuName = "Events/Int Event Channel")]
+public class IntEventChannelSO : EventChannelBaseSO
+{
+    public UnityAction<int> OnEventRaised;
+
+    public void RaiseEvent(int value)
+    {
+        if (OnEventRaised != null)
+            OnEventRaised.Invoke(value);
+    }
+}
